7ebba5f53475eba00ed321b33b00779d40aa634e
Felipe <felipe8297@gmail.com>
2024-02-14 17:13:32 -0300

    feature: adding timeout to simulate api call

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -28,9 +28,8 @@ export function SignUp() {
 
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data)
-
       await new Promise((resolve) => setTimeout(resolve, 2000))
+      console.log(data)
 
       toast.success('Restaurante cadastrado com sucesso!', {
         action: {
