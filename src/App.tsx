e2f0872f84a49dbf8bf46566a3a9f24dfbf50dbb
Felipe <felipe8297@gmail.com>
2024-04-14 13:34:53 -0300

    feat: add axios api types and react query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
   return (
-    <ThemeProvider storageKey="pizza2u-theme" defaultTheme="dark">
-      <HelmetProvider>
-        <Toaster richColors />
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizza2u-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | Pizza 2U" />
-        <RouterProvider router={router} />
-      </HelmetProvider>
-    </ThemeProvider>
+        <Toaster richColors />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   )
 }
